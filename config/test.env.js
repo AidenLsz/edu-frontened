aa308a7ab142365b281ce5f5e10251cb2d6c46b8
yangqing <yqhf@mail.ustc.edu.cn>
2020-03-15 14:28:25 +0800

    some fix

--- config/test.env.js
+++ config/test.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
-  BACKEND_URL: '"DEFAULT_BACKEND_URL"'
+  NODE_ENV: '"testing"'
 })
