12d9c7b535e15d135f09a61415933cdbf8104fa3
yangqing <yqhf@mail.ustc.edu.cn>
2020-03-15 14:00:49 +0800

    test

--- src/main.js
+++ src/main.js
@@ -18,17 +18,18 @@ import "animate.css";
 
 Vue.config.productionTip = false;
 
-if (process.env.BACKEND_URL === "DEFAULT_BACKEND_URL") {
-  Vue.prototype.backendIP =
-    "https://kg-edu-backend-44-production.env.bdaa.pro/v1";
-} else {
-  Vue.prototype.backendIP = process.env.BACKEND_URL;
-}
+// if (process.env.BACKEND_URL === "DEFAULT_BACKEND_URL") {
+//   Vue.prototype.backendIP =
+//     "https://kg-edu-backend-44-production.env.bdaa.pro/v1";
+// } else {
+//   Vue.prototype.backendIP = process.env.BACKEND_URL;
+// }
 // Vue.prototype.backendIP =
 //   "https://kg-edu-backend-44-production.env.bdaa.pro/v1";
 
 // 测试
 // Vue.prototype.backendIP = "http://127.0.0.1:8002";
+Vue.prototype.backendIP = "http://kg-edu-backend-44-review-modify-int-nskg6i.env.bdaa.pro/v1";
 
 Vue.use(ElementUI);
 Vue.use(VueResource);
