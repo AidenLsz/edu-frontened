86e213dea5144178f2fa73b3a365372df1a72db8
Vivian Hong <vivianhong@ViviandeMacBook-Air.local>
2021-05-07 14:04:55 +0800

    legend bug

--- src/views/resourceQuery/components/Tree.vue
+++ src/views/resourceQuery/components/Tree.vue
@@ -83,6 +83,8 @@ export default {
   },
   methods:{
     draw_graph(data){
+      // let margin = ({ left: 60 ,top: 50, right: 120, bottom:120 })
+      let margin = ({ left: 60 ,top: 15, right: 30, bottom:30 })
 
       let width = $('#tree').width()
 
@@ -90,13 +92,12 @@ export default {
       let dx = 30
 
       let tree = d3.tree().nodeSize([dx, dy])
-      let diagonal = d3.linkHorizontal()
-                       .x(d => d.y).y(d => d.x)
+      let diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x)
 
       const root = d3.hierarchy(data)
 
-      // root.x0 = 0
-      // root.y0 = 0
+      root.x0 = 0
+      root.y0 = 0
       root.descendants().forEach((d, i) => {
         d.id = i
         d._children = d.children
@@ -120,7 +121,7 @@ export default {
         .attr('cursor', 'pointer')
         .attr('pointer-events', 'all')
       zoom(svgDOM,svg)
-
+      let [legend,] = addLegend(svgDOM,5,['current','sup','inf'])
       function update (source) {
         const duration = d3.event && d3.event.altKey ? 2500 : 250
         const nodes = root.descendants().reverse()
@@ -136,24 +137,26 @@ export default {
           if (node.x < left.x) left = node
           if (node.x > right.x) right = node
         })
-        let margin = ({ left: 60 ,top: 50, right: 120, bottom:120 })
-        const height = $('#tree').height()
+
+        const height = right.x - left.x + margin.top + margin.bottom
+        // console.log(width,height);
         const transition = svgDOM.transition()
           .duration(duration)
           .attr('viewBox', [-margin.left, left.x - margin.top, width, height])
-          // .attr('viewBox', [0, 0, width, height])
+          // .attr('viewBox', ['25%', '25%', '100%', 380])
           .tween('resize', window.ResizeObserver ? null : () => () => svg.dispatch('toggle'))
-        let [legend,] = addLegend(svg,5,['current','sup','inf'])
-        legend.transition(transition)
-        .attr("transform", (d, i) => `translate(${width-160},${i * 23-90})`)
-
+        legend
+        .transition(transition)
+        .attr("transform", (d, i) => {
+          return `translate(${width-150},${i * 23+left.x-margin.top})`
+        })
         // Update the nodes…
         const node = gNode.selectAll('g')
           .data(nodes, d => d.id)
 
         // Enter any new nodes at the parent's previous position.
         const nodeEnter = node.enter().append('g')
-          // .attr('transform', (d) => `translate(${d.x},${d.y})`)
+          .attr('transform', () => `translate(${source.y0},${source.x0})`)
           .attr('fill-opacity', 0)
           .attr('stroke-opacity', 0)
           .on('click', d => {
@@ -163,6 +166,7 @@ export default {
 
         nodeEnter.append('circle')
           .attr('r', 4)
+          // .attr("stroke", "black")
           .attr('fill', function (d) {
             return color(d.data.community)
           })
@@ -195,10 +199,13 @@ export default {
         // console.log(nodeUpdate) // eslint-disable-line
 
         // Transition exiting nodes to the parent's new position.
+        // const nodeExit =
         node.exit().transition(transition).remove()
           .attr('transform', () => `translate(${source.y},${source.x})`)
           .attr('fill-opacity', 0)
           .attr('stroke-opacity', 0)
+
+        // console.log(nodeExit) // eslint-disable-line
 
         // Update the links…
         const link = gLink.selectAll('path')
