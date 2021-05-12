cedacebdd01c2ca3ef6a5aec3eb816beae390d2b
Vivian Hong <vivianhong@ViviandeMacBook-Air.local>
2021-05-12 14:15:41 +0800

    layout reform

---
+++ src/layout/header.vue
@@ -0,0 +1,330 @@
+<template lang="html">
+  <!-- <div class="">
+    hi
+  </div> -->
+  <el-header style="height: 70px;" v-show="$route.name!='user'">
+    <div id="header-sticky" class="sticky-menu">
+      <!-- <el-row>
+        <el-col :span="4" style="padding-top: 20px; padding-left: 30px">
+          <img src="@/assets/luna_icon.png" alt="Logo" width="150px" style="cursor: pointer" @click="goToMainPage"/>
+        </el-col>
+        <el-col :span="8" :offset="11">
+          <el-row type="flex" justify="end">
+            <el-col :span="4" v-if="username" style="padding-top: 15px;">
+              <el-button type="text" @click="goToMainPage" class="navbar">首页</el-button>
+            </el-col>
+            <el-col :span="4" :offset="1" v-else style="padding-top: 15px;">
+              <el-button type="text" @click="goToMainPage" class="navbar">首页</el-button>
+            </el-col>
+            <el-col :span="4" style="padding-top: 15px;">
+              <el-menu :default-active="activeIndex" mode="horizontal" style=" border-bottom: 3px solid #409EFF; padding-bottom: 10px">
+                <el-submenu index="0">
+                  <template slot="title">
+                    <span :style="Title_Pos()">功能</span>
+                  </template>
+                  <el-submenu index="1">
+                    <template slot="title"><span style="color: black;">资源录入</span></template>
+                    <el-menu-item index="1-1"><span style="color:	Gainsboro;">学习资源</span></el-menu-item>
+                    <router-link to="/inputMarked" :underline="false" @click.native="ToTop">
+                      <el-menu-item index="1-2">
+                        <span style="color: black;">试题资源</span>
+                      </el-menu-item>
+                    </router-link>
+                    <router-link to="/inputPaper" :underline="false" @click.native="ToTop">
+                      <el-menu-item index="1-3">
+                        <span style="color: black;">试卷资源</span>
+                      </el-menu-item>
+                    </router-link>
+                    <el-menu-item index="1-4"><span style="color:	Gainsboro;">知识体系</span></el-menu-item>
+                  </el-submenu>
+                  <el-submenu index="2">
+
+                    <template slot="title"><span style="color: black;">查询</span></template>
+
+                    <router-link to="/exercise" :underline="false" @click.native="ToTop">
+                      <el-menu-item index="2-1">
+                        <span style="color: black;">试题检索</span>
+                      </el-menu-item>
+                    </router-link>
+
+                    <router-link to="/ku" :underline="false" @click.native="ToTop">
+                      <el-menu-item index="2-2">
+                        <span style="color: black;">知识单元检索</span>
+                      </el-menu-item>
+                    </router-link>
+                    <el-menu-item index="2-3"><span style="color: Gainsboro;">学习资源</span></el-menu-item>
+
+                  </el-submenu>
+                  <el-submenu index="3">
+                    <template slot="title"><span style="color: black;">分析</span></template>
+                    <el-menu-item index="3-1"><span style="color: Gainsboro;">学习资源</span></el-menu-item>
+                    <el-menu-item index="3-2" @click="QuestionAnalyseSwitch()" @click.native="ToTop">
+                      <span style="color: black;">试题资源</span>
+                    </el-menu-item>
+                    <el-menu-item index="3-3" @click="PaperAnalyseSwitch()" @click.native="ToTop">
+                      <span style="color: black;">试卷资源</span>
+                    </el-menu-item>
+                  </el-submenu>
+                  <router-link to="/estimate" :underline="false" @click.native="ToTop">
+                    <el-menu-item index="4">
+                        <span style="color: black;">试题属性预估</span>
+                    </el-menu-item>
+                  </router-link>
+                  <router-link to="/similarity" :underline="false" @click.native="ToTop">
+                    <el-menu-item index="5">
+                      <span style="color: black;">相似题预估</span>
+                    </el-menu-item>
+                  </router-link>
+                  <router-link to="/admin" v-if="Get_Priority()" :underline="false" @click.native="ToTop">
+                    <el-menu-item index="5">
+                      <span style="color: red;">管理员页面</span>
+                    </el-menu-item>
+                  </router-link>
+                </el-submenu>
+              </el-menu>
+            </el-col>
+            <el-col :span="4" style="padding-top: 15px;">
+              <el-button type="text" @click="show_members" class="navbar">成员</el-button>
+            </el-col>
+            <el-col :span="5" style="padding-top: 15px;" v-if="username">
+              <el-row>
+                <el-dropdown trigger="hover"  style="padding-top: 8px" class="navbar">
+                  <span class="el-dropdown-link user-inner">
+                    {{ username }}<i class="el-icon-arrow-down el-icon--right"></i>
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item @click.native="logout"
+                      >退出登录</el-dropdown-item
+                    >
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-row>
+            </el-col>
+            <el-col :span="4" style="padding-top: 15px;" v-else>
+                <el-button type="text" @click="login_show" class="navbar">登录</el-button>
+            </el-col>
+            <el-col :span="2" style="padding-top: 15px;">
+              <el-button type="text" @click="register_show" class="navbar">注册</el-button>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row> -->
+    </div>
+  </el-header>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      username:"",
+      activeIndex: "",
+    }
+  },
+  methods:{
+    goToMainPage(){
+      this.$router.push({ path: "/" });
+      this.ToTop();
+    },
+    Title_Pos(){
+      return {
+        "font-size": "18px",
+        "color": "black",
+        "margin-left": "0px"
+      }
+    },
+    Get_Priority(){
+      if(sessionStorage.isAdmin){
+        return true
+      }else{
+        return false
+      }
+    },
+    show_members(){
+      this.$router.push({ path: "/members" });
+      this.ToTop();
+    },
+    login_show() {
+      // this.$router.push({ path: "/login" });
+      this.register_visible = false;
+      this.login_visible = true;
+      setTimeout(()=>{
+        this.$refs.vueImgVerify.handleDraw();},
+      1)
+    },
+    register_show(){
+      this.login_visible = false;
+      this.register_visible = true;
+      setTimeout(()=>{
+        this.$refs.vueImgVerify_reg.handleDraw();},
+      1)
+    },
+    ToTop(){
+      window.scrollTo(0,0);
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.el-header .el-menu--horizontal /deep/ .el-submenu.is-active .el-submenu__title{
+  border-bottom: none;
+}
+
+.el-header .el-menu--horizontal /deep/ .el-submenu .el-submenu__title {
+    height: 42px;
+    line-height: 18px;
+    padding-top: 15px;
+    border-bottom: 2px solid transparent;
+    width: 50%;
+    z-index: 999;
+}
+.el-header .el-menu .el-menu--horizontal {
+    border-bottom: none;
+}
+</style>
+<style>
+.el-header .el-menu--horizontal /deep/ .el-submenu.is-active .el-submenu__title{
+  border-bottom: none;
+}
+
+.el-header .el-menu--horizontal /deep/ .el-submenu .el-submenu__title {
+    height: 42px;
+    line-height: 18px;
+    padding-top: 15px;
+    border-bottom: 2px solid transparent;
+    width: 50%;
+    z-index: 999;
+}
+.el-header .el-menu .el-menu--horizontal {
+    border-bottom: none;
+}
+</style>
+<style scoped lang="scss">
+.menu-area {
+  margin-top: -60px;
+  padding-bottom: -10px;
+  margin-right: 0px;
+  background-color: #fff;
+}
+.logo {
+  margin-left: -100px;
+}
+.main-menu ul li {
+  display: inline-block;
+  position: relative;
+  margin-right: -10px;
+}
+.main-menu ul li:first-child {
+  margin-left: 0;
+}
+.main-menu ul li a {
+  display: block;
+  color: #1a2930;
+  opacity: 0.8;
+  padding: 0;
+  font-size: 18px;
+  font-weight: 700;
+  position: relative;
+  margin-bottom: -20px;
+  text-decoration: none;
+}
+.main-menu ul li:hover > a {
+  color: #000;
+  opacity: 1;
+  font-weight: 900;
+  text-decoration: none;
+}
+.main-menu ul li.active > a {
+  color: #ffd700;
+  opacity: 1;
+  font-weight: 900;
+  text-decoration: none;
+}
+.main-menu ul > li > a::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 5px;
+  width: 0;
+  height: 2px;
+  background: #fff;
+  transition: 0.3s;
+}
+.main-menu ul > li:hover > a::before {
+  width: 100%;
+}
+.main-menu ul li.active a::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 2px;
+  background: #fff;
+  transition: 0.3s;
+}
+.sticky-menu {
+  left: 0;
+  margin: auto;
+  position: fixed;
+  max-height: 70px;
+  top: 0;
+  width: 100%;
+  box-shadow: 0 0px 12px 0 rgba(0, 0, 0, 0.5);
+  z-index: 999;
+  background: #fff;
+  -webkit-animation: 300ms ease-in-out 0s normal none 1 running fadeInDown;
+  animation: 300ms ease-in-out 0s normal none 1 running fadeInDown;
+  -webkit-box-shadow: 0 0px 12px 0 rgba(0, 0, 0, 0.5);
+  margin-top: 0px;
+}
+</style>
+<style scoped>
+.el-dropdown-menu__item:focus,
+.el-dropdown-menu__item:not(.is-disable):hover {
+  background-color: #1a2930 !important;
+  color: #fff !important;
+}
+.el-dropdown-menu__item a {
+  color: #1a2930 !important;
+  text-decoration: none !important;
+}
+.el-dropdown-menu__item:hover a {
+  color: #fff !important;
+  text-decoration: none !important;
+}
+.text-btn {
+  background: transparent;
+  color: #1a2930;
+  border: 0px;
+  margin-left: 30px;
+  margin-right: -35px;
+  font-size: 14px;
+}
+.user-inner {
+  cursor: pointer;
+  color: #1a2930;
+}
+.navbar {
+  font-size: 18px;
+  color: black;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  color: #1a2930;
+  font-size: 18px;
+  font-weight: 900;
+}
+
+.el-header{
+  /*设置内部填充为0，几个布局元素之间没有间距*/
+  padding: 0px 0px 0px 0px;
+    /*外部间距也是如此设置*/
+  margin: 0px;
+  /*统一设置高度为100%*/
+  height: 100%;
+  width: 100%;
+}
+
+</style>
---
+++ src/layout/login.vue
@@ -0,0 +1,142 @@
+<template lang="html">
+  <!-- 登录 -->
+  <el-dialog :visible.sync="visible" width="70%">
+    <el-row>
+      <el-col :span="10" :offset="2">
+        <el-row>
+          <span style="font-weight: bold; color: #47A2FF; font-size: 24px">
+            欢迎使用LUNA智慧教育知识图谱
+          </span>
+        </el-row>
+        <el-row style="margin: 50px 0px 30px 0px;">
+          <el-input
+            type="text"
+            v-model="account"
+            auto-complete="off"
+            placeholder="账号"
+          ></el-input>
+        </el-row>
+        <el-row style="margin: 30px 0px">
+          <el-input
+            type="password"
+            v-model="password"
+            auto-complete="off"
+            placeholder="密码"
+          ></el-input>
+        </el-row>
+        <el-row style="margin: 30px 0px">
+          <el-col :span="15">
+            <el-input
+              type="text"
+              v-model="verifyCode"
+              auto-complete="off"
+              placeholder="请输入验证码"
+            ></el-input>
+          </el-col>
+          <el-col :span="8" :offset="1">
+            <el-row type="flex" justify="end" >
+              <vue-img-verify @getImgCode="getImgCode" ref="vueImgVerify" />
+            </el-row>
+          </el-col>
+        </el-row>
+        <el-row style="margin: 30px 0px">
+          <el-col :span="8">
+            <el-button type="primary" style="width: 8vw" round @click="login">登录</el-button>
+          </el-col>
+          <el-col :span="6" :offset="1">
+            <el-button type="text" style="color: #aaa">忘记密码？</el-button>
+          </el-col>
+          <el-col :span="6" :offset="3">
+            <el-row type="flex" justify="end" >
+              <el-button type="text" @click="register_show">注册新用户</el-button>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="12">
+        <img src="@/assets/login.png" width="350vh" height="350vh"/>
+      </el-col>
+    </el-row>
+  </el-dialog>
+</template>
+
+<script>
+import vueImgVerify from "@/common/components/vue-img-verify.vue";
+
+export default {
+  components: { vueImgVerify },
+  data(){
+    return {
+      account:"",
+      password:"",
+      verifyCode:"",
+      visible:false,
+      imgCode: "",
+    }
+  },
+  watch:{
+    visible(newVal){
+      if(!newVal){
+        this.imgCode = "";
+        this.account = "";
+        this.password = "";
+      }
+      this.visible = newVal;
+    },
+  },
+  methods:{
+    show(){
+      this.visible=true
+      setTimeout(()=>{
+        this.$refs.vueImgVerify.handleDraw();},
+      1)
+    },
+    hide(){
+      this.visible=false
+    },
+    register_show(){
+      this.$emit('register_show');
+    },
+    login() {
+      if(!this.account||!this.password){
+        alert("用户名和密码不能为空");
+        return;
+      }
+      if (this.verifyCode.toUpperCase() !== this.imgCode.toUpperCase()) {
+        alert("验证码错误");
+        return;
+      }
+      this.$http
+        .post(
+          this.backendIP + "/api/login",
+          {
+            username: this.account,
+            password: this.pass
+          },
+          {
+            emulateJSON: true,
+          }
+        )
+        .then(function(data) {
+          if (data.status != 200) { //eslint-disable-line
+            alert("登录失败");
+            return;
+          }
+          sessionStorage.accessToken = data.body.access_token;
+          sessionStorage.user = this.account;
+          sessionStorage.isAdmin = true;
+          this.visible = false;
+          this.$router.push("/user");
+          location.reload();
+        });
+    },
+    getImgCode(code) {
+      this.imgCode = code;
+    },
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
