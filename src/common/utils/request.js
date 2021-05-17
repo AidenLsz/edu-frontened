891c611f3b10a107552514bcba859463c9ca9a50
Vivian Hong <vivianhong@ViviandeMacBook-Air.local>
2021-05-17 12:00:47 +0800

    request timeout

--- src/common/utils/request.js
+++ src/common/utils/request.js
@@ -10,7 +10,7 @@ import store from '@/store'
 const service = axios.create({
   baseURL: Vue.prototype.backendIP, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  timeout: 10000 // request timeout
 })
 
 // request interceptor
