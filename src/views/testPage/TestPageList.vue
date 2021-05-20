dcaa08f69c410e1fd0ffea68c3d8ec1c8a1693a3
HanZiqin <hanziqin@mal.ustc.edu.cn>
2021-05-20 19:54:06 +0800

    issue28-SR - Pic-Cut-Alpha

--- src/views/testPage/TestPageList.vue
+++ src/views/testPage/TestPageList.vue
@@ -1,19 +1,11 @@
 <template>
-  <div class="box">
-      <el-dialog
-        :visible.sync="imgCut"
-        width="65%"
-        :modal-append-to-body="false"
-        :close-on-click-modal="false">
-        <div slot="title" style="margin-bottom: -25px">
-          <label style="font-size: 16px">预览或裁剪要用于搜索的图片</label>
-        </div>
+  <div style="margin: 30px">
         <el-row style="margin: 0px; padding: 0px">
           <el-col :span="15">
-            <el-row type="flex" justify="center" style="margin-bottom: 10px">
+            <el-row type="flex" justify="center" style="margin-bottom: 10px; font-size: 16px; line-height: 30px">
               <label>待裁剪图片</label>
             </el-row>
-            <el-row>
+            <el-row v-if="option.img != ''">
               <VueCropper
                 style="width: 100%; height: 400px"
                 ref="cropper"
@@ -24,15 +16,31 @@
                 :canScale="option.canScale"
               ></VueCropper>
             </el-row>
+            <el-row v-else id="DragItem">
+              <div class="btn_file" style="min-height: 400px">
+                <div style="display: inline-block">
+                  <i class="el-icon-plus" style="margin: 125px auto 20px auto; font-size: 60px"></i>
+                  <p style="font-size: 20px">点击区域或拖拽图片入内以上传</p>
+                </div>
+                <input
+                class="inputSp"
+                  type="file"
+                  @change="pictureSearch($event)"
+                  accept=".jpeg, .png, .jpg"
+                  ref="picSearchInput"
+                  />  
+              </div>
+            </el-row>
           </el-col>
           <el-col :span="8" :offset="1">
-            <el-row type="flex" justify="center" style="margin-bottom: 10px">
+            <el-row type="flex" justify="center" style="margin-bottom: 10px; font-size: 16px; line-height: 30px">
               <label>裁剪结果预览</label>
             </el-row>
             <el-row>
-              <el-image class="pic" :src="imgUrl" alt="" style="width: 100%; height: 200px; border: 1px dashed black" :fit="'contain'">
+              <el-image class="pic" :src="imgUrl" alt="" style="width: 100%; height: 200px; border: 2px dashed black; background: #F8FBFF" :fit="'contain'">
                 <div slot="error">
-                  <span>等待截屏内容</span>
+                  <i class="el-icon-loading" style="margin: 60px auto 10px auto; font-size: 30px"></i>
+                  <p style="font-size: 20px">等待图片内容</p>
                 </div>
               </el-image>
             </el-row>
@@ -84,54 +92,45 @@
                 </el-row>
               </el-col>
             </el-row>
-            <el-row type="flex" justify="center" style="margin-top: 33px">
-              <el-button 
-                type="success" 
-                icon="el-icon-search"
-                @click="getCropData()"
-                plain>
-                确认提交
-              </el-button>
+            <el-row type="flex" justify="start" style="margin-top: 20px; margin-bottom: 20px"> 
+              <el-col :span="12">
+                <el-row type="flex" justify="center">
+                  <el-button 
+                    type="warning" 
+                    icon="el-icon-delete"
+                    @click="clearData()"
+                    plain>
+                    清空内容
+                  </el-button>
+                </el-row>
+              </el-col>
+              <el-col :span="12">
+                <el-row type="flex" justify="center">
+                  <el-button 
+                    type="success" 
+                    icon="el-icon-search"
+                    @click="getCropData()"
+                    plain>
+                    确认提交
+                  </el-button>
+                </el-row>
+              </el-col>
             </el-row>
           </el-col>
-        </el-row>       
-        <!-- <div class="btn">
-        <el-button type="primary" @click="getCropData()">截图</el-button>
-        <el-button
-            type="primary"
-            icon="el-icon-refresh-left"
-            @click="rotateLeft()"
-        ></el-button>
-        <el-button
-            type="primary"
-            icon="el-icon-refresh-right"
-            @click="rotateRight()"
-        ></el-button>
-        <el-button type="primary" @click="refresh()">重置</el-button>
-        </div> -->
-          </el-dialog>
-    <input
-        type="file"
-        @change="pictureSearch($event)"
-        accept=".jpeg, .png, .jpg"
-        ref="picSearchInput"
-        />
+        </el-row>
     </div>
 
 </template>
 <script>
 
-
 export default {
 
     components: {},
     name: "TestPageList",
-    mounted(){
-    },
     data(){
         return {
             option: {
-                img: "https://sucai.suoluomei.cn/sucai_zs/images/20201009165025-bg.png", // 裁剪图片的地址
+                img: "", // 裁剪图片的地址
                 autoCrop: true, //是否默认生成截图框
                 fixedBox: true, //固定截图框大小 不允许改变
                 canMove: false,
@@ -142,7 +141,43 @@ export default {
             imgCut: false
         };
     },
+    mounted() {
+      let upload = document.querySelector('#DragItem');
+      upload.addEventListener('dragenter', this.onDragIn, true);
+      upload.addEventListener('dragleave', this.onDragOut, true);
+      upload.addEventListener('drop', this.onDrop, false);
+    },
     methods: {
+        clearData(){
+          this.imgUrl = "";
+          this.option.img = "";
+        },
+        onDragIn (e) {
+          e.stopPropagation();
+          e.preventDefault();
+        },
+        onDragOut (e) {
+          e.stopPropagation();
+          e.preventDefault();
+        },
+        onDrop (e) {
+          e.stopPropagation();
+          e.preventDefault();
+          this.imgPreview(e.dataTransfer.files);
+          this.$refs.picSearchInput.value = "";
+          this.$refs.cropper.refresh();
+        },
+        //图片预览
+        imgPreview (files) {
+          const _this = this;
+          let read = new FileReader();
+          read.readAsDataURL(files[0]);
+          read.onloadend = function () {  
+            _this.show = true
+            _this.option.img = read.result;
+            _this.imgUrl = read.result;
+          }
+        },
         // 左旋转
         rotateLeft() {
           this.$refs.cropper.rotateLeft();
@@ -205,12 +240,6 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.box {
-  margin: 30px auto 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-}
 .crop,
 .pic {
   width: 600px;
@@ -223,5 +252,24 @@ export default {
 }
 .btn button {
   margin: 10px 0;
+}
+.btn_file {
+  position: relative;
+  min-height: 400px;
+  width: 100%;
+  background-color: #F8FBFF;
+  border-radius: 2px;
+  border: 2px dashed black;
+  cursor: pointer;
+}
+.inputSp {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 400px;
+  width: 100%;
+  overflow: hidden;
+  cursor: pointer;
+  opacity: 0;
 }
 </style>
