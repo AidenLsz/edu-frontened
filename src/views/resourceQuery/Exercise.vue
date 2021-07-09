af3f13500953318650c2568c328266ddf0dad755
Vivian Hong <1044652353@qq.com>
2021-07-09 21:48:25 +0800

    picture uodated

--- src/views/resourceQuery/Exercise.vue
+++ src/views/resourceQuery/Exercise.vue
@@ -582,7 +582,7 @@ export default {
           this.database_list.push({name:data[i]})
           this.database_aim.push(false)
         }
-        // this.database_list[1].name='个人题库'
+        this.database_list[1].name='个人题库'
       })
     },
     // 清除图片
