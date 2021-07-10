f807ef22e6cd428b797905eb830743281be0df0e
Vivian Hong <1044652353@qq.com>
2021-07-10 16:06:38 +0800

    update user group info

--- src/views/manage/userMessage/components/editDialog.vue
+++ src/views/manage/userMessage/components/editDialog.vue
@@ -48,22 +48,26 @@
       <el-row :gutter="20">
         <el-col :span="12">
           <el-form-item label="权限" prop="is_admin" style="margin-bottom: 15px">
-            <el-input
-              type="text"
-              v-model="userData.email"
-              auto-complete="off"
-              placeholder="请输入您的邮箱"
-            ></el-input>
+            <el-select v-model="is_admin" placeholder="请选择" style="width:100%">
+              <el-option
+                v-for="(val,key) in variable.userType"
+                :key="key"
+                :label="val"
+                :value="key">
+              </el-option>
+            </el-select>
           </el-form-item>
         </el-col>
         <el-col :span="12">
           <el-form-item label="状态" prop="status" style="margin-bottom: 15px">
-            <el-input
-              type="text"
-              v-model="userData.phone"
-              auto-complete="off"
-              placeholder="请输入您的手机号码"
-            ></el-input>
+            <el-select v-model="status" placeholder="请选择" style="width:100%">
+              <el-option
+                v-for="(val,key) in variable.status"
+                :key="key"
+                :label="val"
+                :value="key">
+              </el-option>
+            </el-select>
           </el-form-item>
         </el-col>
       </el-row>
@@ -82,6 +86,8 @@ import {
 import {
   commonAjax
 } from '@/common/utils/ajax'
+import * as variable from '@/common/utils/variable'
+
 
 export default {
   props: {
@@ -93,17 +99,22 @@ export default {
     return {
       visible: false,
       formName: 'addUserForm',
-      status:0,
-      is_admin:0,
+      status: 0,
+      is_admin: 0,
+    }
+  },
+  computed: {
+    variable() {
+      return variable
     }
   },
   watch: {
     visible(newVal) {
       if (!newVal) {
         this.$refs[this.formName].resetFields();
-      }else{
-        this.status=this.userData.status
-        this.is_admin=this.userData.is_admin
+      } else {
+        this.status = this.userData.status.toString()
+        this.is_admin = this.userData.is_admin.toString()
       }
       this.visible = newVal;
     },
@@ -113,27 +124,33 @@ export default {
       this.visible = true
     },
     UpdateUserInGroup() {
-      this.$refs[this.formName].validate((valid) => {
-        if (!valid) {
-          return false;
-        }
-        let fd = {
-          username: this.ruleForm.username,
-          password: this.ruleForm.password,
-          phone: this.ruleForm.phone,
-          email: this.ruleForm.email,
-          ug_ID: this.groupid,
-          type: 'user',
-        }
-        commonAjax(this.backendIP + "/api/register", fd).then(() => {
-          this.visible = false;
-          this.$emit('userCreated')
-        }).catch(() => {
-          Message({
-            message: '创建用户失败！',
-            type: 'error',
-            duration: 5 * 1000
-          })
+      if (this.is_admin==this.userData.is_admin &&this.status==this.userData.status) {
+        Message({
+          message: '没有更改任何信息！',
+          type: 'warning',
+          duration: 5 * 1000
+        })
+        return ;
+      }
+      let fd = {
+        user_ID: this.userData.user_ID,
+        ug_ID: this.userData.ug_ID,
+        is_admin: this.is_admin,
+        status: this.status,
+      }
+      commonAjax(this.backendIP + "/api/update_ug_rel", fd).then(() => {
+        this.visible = false;
+        this.$emit('userUpdated')
+        Message({
+          message: '编辑用户信息成功！',
+          type: 'success',
+          duration: 5 * 1000
+        })
+      }).catch(() => {
+        Message({
+          message: '编辑用户信息失败！',
+          type: 'error',
+          duration: 5 * 1000
         })
       })
     },
