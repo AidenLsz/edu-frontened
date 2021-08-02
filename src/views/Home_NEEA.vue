aaca9b3d2b3ed491ebecd9a18c1d556bb686426a
Vivian Hong <1044652353@qq.com>
2021-08-02 15:34:39 +0800

    modify neea banner

--- src/views/Home_NEEA.vue
+++ src/views/Home_NEEA.vue
@@ -526,7 +526,14 @@
     </el-dialog>
 
     <section class="slider-bg" >
-
+      <div class="sys-name">
+        <img src="@/assets/bdaa.png" alt="Logo"  />
+        <span class="text">
+          智慧考试管理系统
+          <br>
+          （DEMO）
+        </span>
+      </div>
     </section>
 
     <!-- about-area -->
@@ -599,7 +606,6 @@ export default {
     };
   },
   mounted() {
-    // this.Init_Bar();
     // data - background
     $("[data-background]").each(function() {
       $(this).css(
@@ -615,14 +621,6 @@ export default {
       this.Count_Type = type;
       this.Redraw_Bar();
     },
-    // Get_Count_Style(type){
-    //   if(type == this.Count_Type){
-    //     return "resourceButton"
-    //   }else{
-    //     return "sleepingButton"
-    //   }
-    // },
-    // 获取首页统计表格的按钮的样式
     QAS(index){
       if(index == 0){
         this.$router.push({ path: "/QuestionAnalyseInput" });
@@ -683,163 +681,28 @@ export default {
     OpenResources(index){
       this.resourceStatus.splice(index, 1, true);
     },
-    // 重绘统计表图
-    // Redraw_Bar(){
-    //   if (BarChart != null && BarChart != "" && BarChart != undefined) {
-    //     BarChart.dispose();//销毁
-    //   }
-    //   BarChart = echarts.init(document.getElementById('data_chart'));
-    //     let option = {
-    //       grid: {
-    //         x: 70,
-    //         y: 90,
-    //         x2: 30,
-    //         y2: 35
-    //       },
-    //       title: {
-    //           text: "各学科数据统计",
-    //           x: "center",
-    //           y: "top",
-    //           textStyle: {
-    //               fontSize: 16,
-    //               fontStyle: 'normal',
-    //               fontWeight: 'bold',
-    //           },
-    //           padding: [5,5,40,25]
-    //       },
-    //       color: ['#409EFD'],
-    //       tooltip : {
-    //           trigger: 'axis',
-    //           axisPointer : {
-    //               type : 'shadow',
-    //               label : {
-    //                   show: true
-    //               }
-    //           },
-    //           textStyle: {
-    //               fontSize: 14,
-    //               fontStyle: 'normal',
-    //               align: 'left'
-    //           },
-    //       },
-    //       calculable: true,
-    //       legend: {
-    //           data: [],
-    //           itemGap: 20,
-    //           x: "right",
-    //           y: "top",
-    //           padding: [5,30,40,5],
-    //           textStyle: {
-    //               fontSize: 14,
-    //               fontStyle: 'normal',
-    //           },
-    //       },
-    //       xAxis : [
-    //       {
-    //           type : 'category',
-    //           data : this.Chart_Data.list_sub,
-    //           axisTick: {
-    //               alignWithLabel: true
-    //           },
-    //           axisLabel:{
-    //               show:true,  //这里的show用于设置是否显示x轴下的字体 默认为true
-    //               interval:0,  //可以设置成 0 强制显示所有标签。如果设置为 1，表示『隔一个标签显示一个标签』，如果值为 2，表示隔两个标签显示一个标签，以此类推。
-    //               textStyle:{   //textStyle里面写x轴下的字体的样式
-    //                   color:'black',
-    //                   fontSize:14
-    //               }
-    //           },
-    //       }
-    //       ],
-    //       yAxis : [
-    //       {
-    //           type : 'value',
-    //           name : '数量',
-    //           axisLabel:{
-    //               show:true,  //这里的show用于设置是否显示y轴下的字体 默认为true
-    //               textStyle:{   //textStyle里面写y轴下的字体的样式
-    //                   color:'black',
-    //                   fontSize:14
-    //               }
-    //           },
-    //           nameTextStyle:{
-    //               color:"black",
-    //               fontSize:14,
-    //               padding:[30, 35, 15, 10]
-    //           }
-    //       }
-    //       ],
-    //       series: []
-    //   };
-    //
-    //   if(this.Count_Type == 'Question'){
-    //     option.legend.data = ['试题']
-    //     option.series = [{
-    //           name:'试题',
-    //           type:'bar',
-    //           barWidth: '30%',
-    //           data: this.Question_Data
-    //       }]
-    //   }else if(this.Count_Type == 'Paper'){
-    //     option.legend.data = ['试卷']
-    //     option.series = [{
-    //           name:'试卷',
-    //           type:'bar',
-    //           barWidth: '30%',
-    //           data: this.Paper_Data
-    //       }]
-    //   }else if(this.Count_Type == 'KU'){
-    //     option.legend.data = ['知识单元']
-    //     option.series = [{
-    //           name:'知识单元',
-    //           type:'bar',
-    //           barWidth: '30%',
-    //           data: this.KU_Data
-    //       }]
-    //   }
-    //   console.log(option.series)
-    //   BarChart.setOption(option);
-    //
-    //   //建议加上以下这一行代码，不加的效果图如下（当浏览器窗口缩小的时候）。超过了div的界限（红色边框）
-    //   window.addEventListener('resize',function() {BarChart.resize()});
-    // },
-    // 初始化统计表图
-    // Init_Bar(){
-    //
-    //   let config = {
-    //       headers: { "Content-Type": "multipart/form-data" }
-    //   };
-    //   let param = new FormData();
-    //
-    //   this.$http
-    //   .post(this.backendIP + "/api/count", param, config, {
-    //     emulateJSON: true
-    //   })
-    //   .then(function(data) {
-    //
-    //     this.Chart_Data = {};
-    //
-    //     this.Paper_Data = [];
-    //     this.Question_Data = [];
-    //     this.KU_Data = [];
-    //
-    //     this.Chart_Data = data.data;
-    //
-    //     this.Num_Paper = this.Chart_Data.num_paper;
-    //     this.Num_Question = this.Chart_Data.num_question;
-    //     this.Num_KU = this.Chart_Data.num_knowledge;
-    //     for(var index = 0; index < this.Chart_Data.num_sub.length; index ++){
-    //       this.Paper_Data.push(this.Chart_Data.num_sub[index].paper);
-    //       this.Question_Data.push(this.Chart_Data.num_sub[index].question);
-    //       this.KU_Data.push(this.Chart_Data.num_sub[index].knowledge);
-    //     }
-    //     this.Redraw_Bar();
-    //   });
-    // }
   }
 };
 </script>
-<style scoped>
+<style scoped lang="scss">
+.sys-name{
+  img{
+    position:relative;
+    height:100px;
+    background: rgba(255,255,255,1);
+    border-radius: 35px;
+    border-radius: 50%;
+    box-shadow:0 0 5px 5px rgba(255,255,255,1) ;
+  }
+  .text{
+    display: inline-block;
+    vertical-align: middle;
+    padding-left: 40px;
+    color:#fff;
+    font-size: 36px;
+    font-weight: 900;
+  }
+}
 .link-bg {
   padding-top: 50px;
   background: #EEF5FE;
@@ -851,13 +714,16 @@ export default {
   z-index: 1;
 }
 .slider-bg {
-  background: url("../assets/bg.png");
+  background: url("../assets/bg_pure.png");
   background-size: 100%;
   position: relative;
   min-height: 653px;
   background-position: center;
   background-size: cover;
   z-index: 1;
+  justify-content:center;
+  align-items:center;
+  display:-webkit-flex;
 }
 .slider-bg::before {
   content: "";
@@ -929,13 +795,7 @@ export default {
 .search-area {
   padding-top: 100px;
 }
-/* }
-.link-card {
-  height: 360px;
-  overflow: inherit;
-  background-color: #1a2930;
-  color: #fff;
-} */
+
 .card_title {
   font-weight: 900;
   font-size: 17px;
@@ -961,41 +821,6 @@ a {
   width:100%;
   margin:24px 0
 }
-.resourceButton{
-  background: #48a6f3;
-  color: white;
-}
-.el-button:focus{
-  background: #48a6f3;
-  color: white;
-}
-.el-button:active{
-  background: #48a6f3;
-  color: white;
-}
-.el-button:hover{
-  background: #98caf3;
-  color: white;
-}
-.sleepingButton{
-  background: #F8FBFF;
-  color: rgb(122, 122, 122);
-}
 
-/* .partData{
-  height: 120px;
-  background: #EEF5FE;
-  box-shadow: 0 0 60px 0 rgba(0, 0, 0, 0.07);
-  -webkit-box-shadow: 0 10px 15px rgba(25, 25, 25, 0.1);
-  border-radius: 10px;
-}
-.data_chart{
-   border-radius: 10px;
-   width: 67%;
-   height:300px;
-   padding-top: 20px;
-   margin-left: 16.5%;
-   border: 3px solid #EEF5FE;
-   margin-bottom: 40px;
-} */
+
 </style>
