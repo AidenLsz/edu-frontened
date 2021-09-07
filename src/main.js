3ed4dbc6454ef19cf6ab32935bf6865a9be0880f
Vivian Hong <1044652353@qq.com>
2021-09-07 09:28:20 +0800

    input test

--- src/main.js
+++ src/main.js
@@ -33,6 +33,7 @@ if (process.env.NODE_ENV === "production") {
 } else {
   //dev 开发环境
   Vue.prototype.backendIP = "https://kg-edu-backend-44-production.env.bdaa.pro/v1";
+  Vue.prototype.backendIP = "http://localhost:5000";
   // Vue.prototype.backendIP = "https://kg-edu-backend-44-review-user-u9mmdq.env.bdaa.pro/v1";
 }
 
