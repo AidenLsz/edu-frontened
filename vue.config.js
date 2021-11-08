ff88eb15a4ab0821989a653a3df110439f8d78cd
HanZiqin <hanziqin@mal.ustc.edu.cn>
2021-11-08 19:41:01 +0800

    Replace-Icon

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,14 @@ module.exports = {
         Popper: ['popper.js', 'default']
       })
     ]
+  },
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
   }
 }
