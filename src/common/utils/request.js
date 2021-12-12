9583b3d0871886c5c5d99c9440148d6c55304bf4
Vivian Hong <vivianhong@ViviandeMacBook-Pro.local>
2021-12-12 21:05:59 +0800

    send email code in the backend

--- src/common/utils/request.js
+++ src/common/utils/request.js
@@ -58,20 +58,6 @@ service.interceptors.response.use(
         type: 'error',
         duration: 5 * 1000
       })
-
-      // // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      // if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-      //   // to re-login
-      //   MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-      //     confirmButtonText: 'Re-Login',
-      //     cancelButtonText: 'Cancel',
-      //     type: 'warning'
-      //   }).then(() => {
-      //     store.dispatch('user/resetToken').then(() => {
-      //       location.reload()
-      //     })
-      //   })
-      // }
       return Promise.reject(new Error(res.data || 'Error'))
     } else {
       return res
@@ -84,7 +70,7 @@ service.interceptors.response.use(
     //   type: 'error',
     //   duration: 5 * 1000
     // })
-    return Promise.reject(error)
+    return Promise.reject(error.response)
   }
 )
 
